09ab2066839fecfe2b4d2e9414d3ab2705597c0d
RKRedfern <rkredfern@gmail.com>
2021-04-27 08:51:16 -0600

    commit tree 5f1ad7b0b0bd9e40d83f435787b1670b482d2eee

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,40 @@
 // Code MovieReviews Here
+import React from 'react'
+
+const Review = ({
+    headline, 
+    byline, 
+    link,
+    summary_short
+}) => {
+    return (
+    <div
+        key={headline}
+        className="review"
+        >
+        <header>
+            <a
+            className="review-link"
+            href={link.url}
+            >
+            {headline}
+            </a>
+            <br/>
+            <span className="author">{byline}</span>
+        </header>
+        <blockquote>{summary_short}</blockquote>
+    </div>
+    );
+};
+
+const MovieReviews = ({ reviews }) => <div className="review-list"> {reviews.map(Review)}</div>;
+
+MovieReviews.defaultProps = {
+    reviews: []
+};
+
+export default MovieReviews;
+
+//This component serves as the presentation component that is responsible for displaying a list of movie reviews on a page.
+//The two other Container components will be responsible for wrapping this component and displaying it. 
+//This component should be stateless and functional
